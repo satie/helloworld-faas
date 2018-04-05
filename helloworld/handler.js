391f2abcbf4f463134b47886cf39a3bc5b416150
satie <satiesharma@gmail.com>
2018-04-05 19:51:21 -0400

    Use request module instead of http

--- helloworld/handler.js
+++ helloworld/handler.js
@@ -1,54 +1,9 @@
 "use strict"
 const http = require('http');
+const request = require('request');
 
 module.exports = (context, callback) => {    
-    world().then((name) => {
-        hello(name).then((data) => {
-            callback(undefined, data);
-        });
-    });    
+    request.get('http://localhost:8080/function/world').pipe(request.post('http://localhost:8080/function/hello', (e, r, b) => {
+        callback(undefined, b);
+    }));    
 }
-
-const hello = (name) => {
-    return new Promise(function(resolve, reject) {
-        const options = {
-            hostname: 'localhost',
-            port: 8080,
-            path: '/function/hello',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'text/data',
-                'Content-Length': Buffer.byteLength(name)
-            }
-        };
-        const req = http.request(options, (res) => {
-            let body = '';
-            res.on('data', data => {
-                body += data;
-            });
-            res.on('end', () => {
-                resolve(body);
-            }); 
-        });
-        req.on('error', (e) => {
-            reject(e);
-        });
-
-        req.write(name);
-        req.end();
-    });
-};
-
-const world  = () => {
-    return new Promise(function(resolve, reject) {
-        http.get('http://localhost:8080/function/world', res => {
-            let body = '';
-            res.on('data', data => {
-                body += data;
-            });
-            res.on('end', () => {            
-                resolve(body);
-            });
-        });
-    }); 
-};
