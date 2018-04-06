385e08575f7f3ff90f1980f1c7e193ac1ef56fda
satie <satiesharma@gmail.com>
2018-04-06 18:05:58 -0400

    Changed faas host to 'gateway' from 'localhost' as recommended here https://github.com/openfaas/faas/issues/633

--- helloworld/handler.js
+++ helloworld/handler.js
@@ -3,7 +3,7 @@ const http = require('http');
 const request = require('request');
 
 module.exports = (context, callback) => {    
-    request.get('http://localhost:8080/function/world').pipe(request.post('http://localhost:8080/function/hello', (e, r, b) => {
+    request.get('http://gateway:8080/function/world').pipe(request.post('http://gateway:8080/function/hello', (e, r, b) => {
         callback(undefined, b);
     }));    
 }
